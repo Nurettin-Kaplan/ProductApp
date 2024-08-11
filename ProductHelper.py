6bf889ae37ad884719b7e43a5b085aa0de4f2836
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-08-11 16:54:59 +0300

    The first method of the 'ProductHelper' class has been written, and changes have been made to the 'Products.txt' file.

--- ProductHelper.py
+++ ProductHelper.py
@@ -1,10 +1,19 @@
-import Product
-
+from Product import Product
 class ProductHelper(Product):
+    @staticmethod
+    def create_item_from_text():
+        products = []
 
+        with open("Products.txt", "r") as products_file:
+            for line in products_file:
+                id, name, price, quantity = line.strip().split(", ")
+
+                name = name.strip()
+                price = float(price.strip())
+                quantity = int(quantity.strip())
+
+                product = Product(name, price, quantity)
+                products.append(product)
     @staticmethod
-    def create_item_from_text(self):
-        pass
-    @staticmethod
-    def get_total_balance(self):
+    def get_total_balance():
         pass
