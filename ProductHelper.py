67b45357aff0032a257e92292c477af6bd8f6766
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-08-11 15:45:24 +0300

    The 'ProductHelper' class has been created.

---
+++ ProductHelper.py
@@ -0,0 +1,10 @@
+import Product
+
+class ProductHelper(Product):
+
+    @staticmethod
+    def create_item_from_text(self):
+        pass
+    @staticmethod
+    def get_total_balance(self):
+        pass
